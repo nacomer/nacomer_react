34a3eed8333d042942bee24ef81159544cda9b64
土井原康平 <k-doihara@doiharakouheinoMacBook-Pro.local>
2020-11-20 21:16:02 +0900

    fix #60 by warning

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -17,30 +17,31 @@ export default function Detail(props) {
     subPicture: [],
   };
   const [detailJson, setDetailJson] = useState(initDataJson);
-  const hobbyDetail = {
-    name: "tennis",
-    mainPicture: "./main_tennis.jpg",
-    description:
-      "始めたばかりの頃はボールを打つこと自体も楽しいですし、上達してきたら習得した技術で作戦を立てて、相手の裏をかいたりボールをコントロールしたりするのも楽しいです。",
-    cost: 10000,
-    periodID: 1,
-    Youtube: [
-      { youtubeURL: "https://www.youtube.com/embed/XLSamYktmUo" },
-      { youtubeURL: "https://www.youtube.com/embed/27eyrDivrz0" },
-    ],
-    subPicture: [
-      { subpictureURL: "../../test_image/img1.jpg" },
-      { subpictureURL: "../../test_image/img2.jpg" },
-    ],
-  };
   
   useEffect(() => {
     const getHobbyDetail = async () => {
+      const hobbyDetail = {
+        name: "tennis",
+        mainPicture: "./main_tennis.jpg",
+        description:
+          "始めたばかりの頃はボールを打つこと自体も楽しいですし、上達してきたら習得した技術で作戦を立てて、相手の裏をかいたりボールをコントロールしたりするのも楽しいです。",
+        cost: 10000,
+        periodID: 1,
+        Youtube: [
+          { youtubeURL: "https://www.youtube.com/embed/XLSamYktmUo" },
+          { youtubeURL: "https://www.youtube.com/embed/27eyrDivrz0" },
+        ],
+        subPicture: [
+          { subpictureURL: "../../test_image/img1.jpg" },
+          { subpictureURL: "../../test_image/img2.jpg" },
+        ],
+      };
       //const hobbyDetail = await fetch(process.env.URL + "/api/hobby/" + props.hobbyId)
       setDetailJson(hobbyDetail);
     };
     getHobbyDetail();
   },[]);
+
   return (
     <div>
       <DetailHeader detailJson={detailJson}/>
