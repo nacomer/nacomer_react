fa5e8904d14b3474583ddb7c524cacfdbd013681
片岡巧 <taku@kataokatakuminoMacBook-Air.local>
2020-11-20 19:46:34 +0900

    fix header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import logo from "../image/logo.png";
 import "../styles/header.css";
 
-export default function Header() {    
-    return <img src={logo} className="logo" />
+export default function Header() {
+    return (
+        <img
+            src={logo}
+            alt="logo"
+            className="logo"
+        />
+    )
+
 }
