c589bd7d83b2809edae7cfc2f8bb0d05e45af26f
片岡巧 <taku@kataokatakuminoMacBook-Air.local>
2020-11-20 20:35:32 +0900

    Add pics

--- src/components/list/ListView.jsx
+++ src/components/list/ListView.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import "../../styles/list.css"
 
 export default function ListView() {
+    const apiHobby = [
+        {
+            name: "golf",
+            picture: "./img/golf.png",
+            periodID: 1
+        },
+        {
+            name: "camp",
+            picture: "./img/camp.png",
+            periodID: 2
+        }
+    ]
+
+
     return (
-        <div className="listView">
+        <div
+            className="listView"
+        >
             <p>ListView</p>
-        </div>)
+        </div>
+    )
 }
