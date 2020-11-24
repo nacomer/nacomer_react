f725d4794bdc883f07c8abc5bced913a5192aebb
taku <katataku.dev@gmail.com>
2020-11-24 17:37:38 +0900

    fix table

--- src/components/detail/detailIntro/DetailIntro.jsx
+++ src/components/detail/detailIntro/DetailIntro.jsx
@@ -5,19 +5,23 @@ import DetailIntroYoutubeItem from './DetailIntroYoutubeItem';
 export default function DetailIntro(props) {
   return (
 
-<>
-    <table width="100%">
-      <caption>オススメ動画</caption>
-      {props.detailJson.Videos.map((value, index) => {
-        return <DetailIntroYoutubeItem url={value.videoURL} description={value.description} key={index} />
-      })}
-    </table>
-    <table width="100%">
-    <caption>オススメグッズ・イベント</caption>
-       {props.detailJson.SubPictures.map((value, index) => {
-        return <DetailIntroGoodsItem url={value.subPicture} description={value.description} key={index} />
-      })}
-    </table>
-</>
+    <>
+      <table width="100%">
+        <caption>オススメ動画</caption>
+        <tbody>
+          {props.detailJson.Videos.map((value, index) => {
+            return <DetailIntroYoutubeItem url={value.videoURL} description={value.description} key={index} />
+          })}
+        </tbody>
+      </table>
+      <table width="100%">
+        <caption>オススメグッズ・イベント</caption>
+        <tbody>
+          {props.detailJson.SubPictures.map((value, index) => {
+            return <DetailIntroGoodsItem url={value.subPicture} description={value.description} key={index} />
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
