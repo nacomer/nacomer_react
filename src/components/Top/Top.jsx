aee3c1470e4163299f05460b8e5ac737165a2f84
土井原康平 <k-doihara@doiharakouheinoMacBook-Pro.local>
2020-11-25 15:02:38 +0900

    favicon

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -87,7 +87,8 @@ export default function Top(props) {
   return (
     <div>
       <img className="topImage" src="./image/friends.jpg" />
-      <p className="topCatchCopy">仲間との出会いへ</p>
+      <p className="topCatchCopy">自分らしくいられる趣味を見つけよう</p>
+      <p className="topCatchCopySub">〜 be NAture, as a new COMER 〜</p>
       <div className="log-form">
         <input
           type="text"
@@ -106,7 +107,6 @@ export default function Top(props) {
         <button type="submit" className="topBtn" onClick={clickLogin}>
           Login
         </button>
-        <br />
         <button
           type="submit"
           className="topBtn"
