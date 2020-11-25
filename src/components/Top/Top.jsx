00ad8e0f1461de31243d853e016c800c59e32bbf
土井原康平 <k-doihara@doiharakouheinoMacBook-Pro.local>
2020-11-25 17:20:43 +0900

    natural

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -88,7 +88,7 @@ export default function Top(props) {
     <div>
       <img className="topImage" src="./image/friends.jpg" />
       <p className="topCatchCopy">自分らしくいられる趣味を見つけよう</p>
-      <p className="topCatchCopySub">〜 be NAture, as a new COMER 〜</p>
+      <p className="topCatchCopySub">〜 be NAtural, as a newCOMER 〜</p>
       <div className="log-form">
         <input
           type="text"
