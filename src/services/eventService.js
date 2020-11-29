41f3363e1dc27994bf9d4b1498ed8713feb8acf6
kazzzzzzaki <kazuakiu0327@gmail.com>
2020-11-29 23:48:08 +0900

    yarn lint

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -10,10 +10,28 @@ export default class EventService {
         'x-googleid': userData.googleId,
       },
     };
-
     const eventInfoRes = await axios
       .get(`${process.env.REACT_APP_URL}/v1/events/${eventId}`, header)
       .catch('イベント情報の取得に失敗しました。');
     return eventInfoRes;
+  };
+
+  // イベントへの参加登録API
+  participateEvent = async (userData, eventId) => {
+    const header = {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': userData.tokenId,
+        'x-googleid': userData.googleId,
+      },
+    };
+    const body = {
+      eventId,
+    };
+
+    const participantsRes = await axios
+      .post(`${process.env.REACT_APP_URL}/v1/participants`, body, header)
+      .catch('イベントの参加登録に失敗しました。');
+    return participantsRes;
   };
 }
