3a5055238ade17ac55bc6291d9e0c9fb59576434
h-tsuboi918 <hiroki.tsuboi.918@gmail.com>
2020-12-01 21:34:56 +0900

    [add] ios-vh

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'ui-neumorphism/dist/index.css';
+import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
+
+viewportUnitsBuggyfill.init();
 
 ReactDOM.render(<App />, document.getElementById('root'));
