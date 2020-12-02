4cfe24f83d5f0c0f6a5290e9695b65e8c869f0f6
h-tsuboi918 <hiroki.tsuboi.918@gmail.com>
2020-12-02 09:23:22 +0900

    [fix] scroll and etc

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -25,6 +25,7 @@ export default function Top(props) {
         props.setLoginSuccess(true);
         // save cookie
         props.setCookie('loginUser', JSON.stringify(resLoginUser));
+        location.reload();
       }
     }
   };
